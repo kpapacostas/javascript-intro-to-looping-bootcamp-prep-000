7e914c15ce13288d6d10fceb3d8e5d952a564d5c
Katerina  Papacostas <katpapacostas@gmail.com>
2017-02-02 20:04:08 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,27 @@
+
+// for (let i = 1, i < 25, i++) {
+//   if (i = 1) {
+//     console.log( "I am" + i + "strange loop" )
+//   } else {
+//     console.log( "I am" + ${i} + "strange loop" )
+//   }
+// }
+
+while (number > 0) {
+  if (number > 0) {
+    console.log(--number)
+  }
+  else {
+    return 'done'
+  }
+}
+
+function maybeTrue () {
+  return Math.random () >= 0.5
+}
+
+function doWhileLoop(array) {
+do {
+  console.log (array.length > 0 &&)
+} while (maybeTrue())
+}  
